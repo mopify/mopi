4d0509900d6aa30b75a7836c9fa6a4d0c9a969a8
Scott Lowe <scott.code.lowe@gmail.com>
2016-02-23 17:03:40 +0000

    REL: Add short copy of GPLv3 license to source code files
    Add copy to mopi.sh and mopi.m.

--- mopi.m
+++ mopi.m
@@ -43,6 +43,22 @@
 %   are decompressed. By default, this is PACKAGES_FOLDER/.cache.
 %
 %   See also INSTALL_SINGLE.
+
+% Copyright (C) 2016  Scott C. Lowe <scott.code.lowe@gmail.com>
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 function mopi(input, packages_folder, fixpath, download_folder)
     % Input handling --------------------------------------------------
     if nargin<1 || isempty(input)
